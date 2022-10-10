714a9462fa270517768471ff7558b68043ddd1ad
adamsyy <adamoommenjacob@gmail.com>
2022-10-10 19:16:31 +0530

    tsst

--- my-app/pages/api/[tokenId].js
+++ my-app/pages/api/[tokenId].js
@@ -1,9 +1,10 @@
-export default function handler(req, res) {
+
+  export default function handler(req, res) {
     // get the tokenId from the query params
     const tokenId = req.query.tokenId;
     // As all the images are uploaded on github, we can extract the images from github directly.
     const image_url =
-      "https://nftcollection-adamsy.vercel.app/mascot4.png";
+      "https://raw.githubusercontent.com/LearnWeb3DAO/NFT-Collection/main/my-app/public/cryptodevs/";
     // The api is sending back metadata for a Crypto Dev
     // To make our collection compatible with Opensea, we need to follow some Metadata standards
     // when sending back the response from the api
@@ -11,6 +12,6 @@ export default function handler(req, res) {
     res.status(200).json({
       name: "Pixie Token #" + tokenId,
       description: "Get exclusive perks by minting the pixie NFTs",
-      image: image_url,
+      image: image_url + tokenId + ".svg",
     });
   }
